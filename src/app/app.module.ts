934d9f036f993d0a33813732624d141f613bf932
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2022-10-07 14:34:18 +0530

    first-commit-with-two-components-and-moduleUpdates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([{path:'',component:ProductListComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
