d4de58288acc5a86370141188f4f306761f3aa97
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2022-10-08 11:22:33 +0530

    url-path-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{path:'',component:ProductListComponent}])
+    RouterModule.forRoot([
+      {path:'',component:ProductListComponent},
+      {path:'products/:productId', component: ProductDetailsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
